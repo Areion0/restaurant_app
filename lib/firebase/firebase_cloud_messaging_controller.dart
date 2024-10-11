c226d44880e3212c5fda9efa3c9e00685e39dfff
Vaggelis Chaldaios <23741094+Areion0@users.noreply.github.com>
2024-10-12 01:49:56 +0300

    Push Notifications - Change to live server Uri

--- lib/firebase/firebase_cloud_messaging_controller.dart
+++ lib/firebase/firebase_cloud_messaging_controller.dart
@@ -7,19 +7,25 @@ class FirebaseCloudMessagingController {
   static Future<void> sendNotificationToUser(String userID) async {
     Logger logger = Logger();
 
+    Uri endpointUri = Uri.parse("https://restaurant-app-backend.netlify.app/api/send-notification");
+
     try {
       logger.i("Sending notification to user with id:$userID");
-      await http.post(
-        Uri.parse("http://10.0.2.2:3000/send-notification"),
+      await http
+          .post(
+        endpointUri,
         headers: <String, String>{
           'Content-Type': 'application/json',
         },
         body: jsonEncode(<String, dynamic>{
           "userID": userID,
         }),
-      ).then((value) {
-        logger.i(value.body);
-      },);
+      )
+          .then(
+        (value) {
+          logger.i(value.body);
+        },
+      );
     } on Exception catch (e, s) {
       logger.e("Failed to send notification to user: $e");
       logger.e(s);
